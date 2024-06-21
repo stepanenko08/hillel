107163640b95761eb185c33947da92eacf58ec4d
Vlad Step <stepanenkovlad2008@gmail.com>
2024-06-21 14:24:38 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// const number = [2, 5, 8, 3, 12, 7, 6, 9, 4];
+
+// for (let i = 0; i < number.length; i++) {
+//     const el = number[i];
+
+//     if (el > 3 && el < 10) {
+//         console.log(el);
+//     }
+// }
+
+
